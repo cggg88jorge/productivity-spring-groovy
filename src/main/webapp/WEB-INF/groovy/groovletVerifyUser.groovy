bd104f727ada29d2ef605b513f11a61a6d1fd19b
neodevelop <neodevelop@gmail.com>
2015-10-23 10:43:09 -0500

    Recuperando el appctx con el contexto de servlet, explorando y obteniendo un bean

--- src/main/webapp/WEB-INF/groovy/groovletVerifyUser.groovy
+++ src/main/webapp/WEB-INF/groovy/groovletVerifyUser.groovy
@@ -1,5 +1,20 @@
 import com.makingdevs.CustomerRepository
+import org.springframework.web.context.support.*
+import org.springframework.web.context.*
+
 
 println "en el groovlet"
 
+WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(context)
 
+println "<br>"
+println ctx.dump()
+ctx.metaClass.metaMethods.each {
+  println it.name
+  println "<br>"
+}
+println "<br>"
+println ctx.properties
+println "<br>"
+
+//ctx.getBean(CustomerRepository)
