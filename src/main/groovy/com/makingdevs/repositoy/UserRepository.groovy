dcfcc7fcff18619a64134fa144e758f434e1106f
Jorge Acosta Lemus <jorge@makingdevs.com>
2015-10-25 17:22:56 -0600

    creando repository de user para la perisistencia con jps

---
+++ src/main/groovy/com/makingdevs/repositoy/UserRepository.groovy
@@ -0,0 +1,8 @@
+package com.makingdevs.repository
+
+import org.springframework.data.mongodb.repository.MongoRepository
+import com.makingdevs.domain.User
+
+interface UserRepository extends MongoRepository<User, String> {
+
+}
