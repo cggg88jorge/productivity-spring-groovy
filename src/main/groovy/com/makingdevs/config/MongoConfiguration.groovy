9e013c4d5dcd0b30b6755aa84972b84661175f01
Jorge Acosta Lemus <jorge@makingdevs.com>
2015-10-25 13:28:45 -0600

    de mueve la clase de configuracion

--- src/main/groovy/com/makingdevs/config/MongoConfiguration.groovy
+++ src/main/groovy/com/makingdevs/config/MongoConfiguration.groovy
@@ -1,12 +1,14 @@
-package com.makingdevs
+package com.makingdevs.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
 import com.mongodb.*
 
 @Configuration
-@EnableMongoRepositories(basePackages = "com.makingdevs")
+@ComponentScan(basePackages = ["com.makingdevs.service"])
+@EnableMongoRepositories(basePackages = "com.makingdevs.repository")
 class MongoConfiguration extends AbstractMongoConfiguration {
 
     String getDatabaseName() {
