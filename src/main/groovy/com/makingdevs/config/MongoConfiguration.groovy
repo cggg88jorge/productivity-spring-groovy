1d97378418315e1983229d08786178dcd0adcc8c
Jorge Acosta Lemus <jorge@makingdevs.com>
2015-10-25 15:49:49 -0600

    agregando la configuracion externalizada al proycto

--- src/main/groovy/com/makingdevs/config/MongoConfiguration.groovy
+++ src/main/groovy/com/makingdevs/config/MongoConfiguration.groovy
@@ -1,9 +1,10 @@
 package com.makingdevs.config
 
-import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Bean
 import com.mongodb.*
 
 @Configuration
@@ -11,15 +12,24 @@ import com.mongodb.*
 @EnableMongoRepositories(basePackages = "com.makingdevs.repository")
 class MongoConfiguration extends AbstractMongoConfiguration {
 
-    String getDatabaseName() {
-        "local"
-    }
+  private static final String PROPERTIES_FILE_URL = "${System.properties['user.home']}/.projects_config/productivity-spring-groovy.properties"
 
-    Mongo mongo() throws Exception {
-        new MongoClient("127.0.0.1", 27017)
-    }
+  String getDatabaseName() {
+    "local"
+  }
 
-    String getMappingBasePackage() {
-        "com.makingdevs"
-    }
+  Mongo mongo() throws Exception {
+    new MongoClient("127.0.0.1", 27017)
+  }
+
+  String getMappingBasePackage() {
+    "com.makingdevs"
+  }
+
+  @Bean
+  def properties() {
+    def file = new File(PROPERTIES_FILE_URL)
+    new ConfigSlurper().parse(file.toURI().toURL())
+  }
+
 }
