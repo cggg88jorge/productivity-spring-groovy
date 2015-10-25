fb1bdd91670dc5dc5a65d2bbb39d62a537dd3d53
Jorge Acosta Lemus <jorge@makingdevs.com>
2015-10-25 17:23:22 -0600

    comentando posible proceso ademas de ajusta el parametro del metodo

--- src/main/groovy/com/makingdevs/service/impl/MigrationServiceImpl.groovy
+++ src/main/groovy/com/makingdevs/service/impl/MigrationServiceImpl.groovy
@@ -7,8 +7,15 @@ import org.springframework.stereotype.Service
 @Service
 class MigrationServiceImpl implements MigrationService{
 
-  def initMiration(){
+
+
+  def initMiration(String username){
     println "empieza la migracion"
+    //http://jsonplaceholder.typicode.com/users/1 ir po el usuario
+    //buscar en la base
+    // ir pot sus comentarios
+    //generar estrucutra
+    // gurdarlos
   }
 
 }
