e1c8d80e500349accfe4c27cd5f166ee3991c6a4
Jorge Acosta Lemus <jorge@makingdevs.com>
2015-10-25 13:24:43 -0600

    modificando lo que se muestra en el groovlet

--- src/main/webapp/WEB-INF/groovy/groovletVerifyUser.groovy
+++ src/main/webapp/WEB-INF/groovy/groovletVerifyUser.groovy
@@ -1,18 +1,14 @@
-import com.makingdevs.CustomerRepository
 import org.springframework.web.context.support.*
 import org.springframework.web.context.*
-
 
 println "en el groovlet"
 
 WebApplicationContext ctx = WebApplicationContextUtils.getWebApplicationContext(context)
 
 println "<br>"
-println ctx.dump()
-ctx.metaClass.metaMethods.each {
-  println it.name
-  println "<br>"
-}
+println ctx.getBean("customerRepository")
+println "<br>"
+println ctx.getBean("delegateActor")
 println "<br>"
 println ctx.properties
 println "<br>"
